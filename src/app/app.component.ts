b1d35fa886ef9b78ea4b3a4e5104c5a1487b1f33
alejizz <estefannyseguro.777@gmail.com>
2017-06-04 12:24:40 -0500

    add solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  count = 0;
+  imgSource = '../favicon.ico';
+  enabled = true;
 }
